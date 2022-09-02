461c46ec184ca35a1c8f9aa8f82998033a3a4a57
Rogisimo <rogisonfleek@gmail.com>
2022-09-02 19:17:14 +0200

    HTML + CSS + COMPONENTS

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+//Importing components
+import Form from "./components/Form"
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>React Todo List</h1>
+      </header>
+      <Form/>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
