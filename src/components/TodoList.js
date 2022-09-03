899ab70ce98a14640ee276cb5aa2b3bf5cd008ac
Rogisimo <rogisonfleek@gmail.com>
2022-09-03 21:05:18 +0200

    This is a new commit for what I originally planned to be amended

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,19 @@ import React from "react";
 //Importing
 import Todo from "./Todo"
 
-const TodoList = ({todos}) =>{
+const TodoList = ({todos, setTodos}) =>{
 
     return(
         <div className="todo-container">
             <ul className="todo-list">
-                {todos.map(todo => (
-                    <Todo text={todo.text}/>
+                {todos.map((todo) => (
+                    <Todo 
+                    setTodos={setTodos} 
+                    todos={todos}
+                    key={todo.id}
+                    todo={todo}
+                    text={todo.text}
+                    />
                 ))}
             </ul>
         </div>
