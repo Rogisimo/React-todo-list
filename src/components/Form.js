9a20cab1f3149399a03ceeef916e2a5405b3d564
Rogisimo <rogisonfleek@gmail.com>
2022-09-04 15:41:09 +0200

    Filtering and local storage (not working properly)

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-const Form = ({setInputText, todos, setTodos, inputText}) =>{
+const Form = ({setInputText, todos, setTodos, inputText, status, setStatus}) =>{
     const inputTextHandler = (e) => {
         setInputText(e.target.value)
     }
@@ -17,6 +17,10 @@ const Form = ({setInputText, todos, setTodos, inputText}) =>{
         ]);
         setInputText("");
     };
+
+    const statusHandler = (e) =>{
+      setStatus(e.target.value);
+    }
     return(
         <form>
       <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
@@ -24,7 +28,7 @@ const Form = ({setInputText, todos, setTodos, inputText}) =>{
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
